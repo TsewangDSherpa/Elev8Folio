124129b798b9af49d26e1a13df0d399b4d53beb2
Tsewanghyolmo01 <tds22@njit.edu>
2023-11-10 15:16:14 -0500

    working on homepage

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import Header from "./Components/Header";
 import SumeNav from "./Components/SumeNav";
 import Sume from "./Components/Sume";
 import { useState } from "react";
+import Homepage from "./Components/Homepage";
 
 function App() {
-  const [Color, setColor] = useState("");
-  
-  const getColor = (color) => {
-    
-    setColor(color);
-  
-  };
-  console.log("The RBG" )
+  // const [Color, setColor] = useState("");
+
+  // const getColor = (color) => {
+
+  //   setColor(color);
+
+  // };
+  // console.log("The RBG" )
+  // return (
+  //   <>
+  //     <Header getColor={getColor} />
+  //     <div className="Body">
+  //       <SumeNav bgColor={Color} />
+  //       <Sume />
+  //     </div>
+  //   </>
+  // );
+
   return (
     <>
-      <Header getColor={getColor} />
-      <div className="Body">
-        <SumeNav bgColor={Color} />
-        <Sume />
-      </div>
+      <Homepage></Homepage>
     </>
   );
 }
