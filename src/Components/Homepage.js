2aea44e3d61f63fa8cce86b160d5426c4afb6e6d
Norsang Nyandak <TNorsang>
2023-11-10 15:24:35 -0500

    added homepage divs

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,5 +1,22 @@
 function Homepage() {
-  return <div>Home</div>;
+  return (
+    <div className="HomePageHolder">
+      <div className="Header">
+        <h2 className="Title">Elev8Folio</h2>
+        <div>
+          <p> Made for Students</p>
+          <p> From two Students</p>
+        </div>
+      </div>
+      <div className="Body">
+        <div className="StepOne">
+          <p>
+            Step 1: <span> Upload Resume </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
